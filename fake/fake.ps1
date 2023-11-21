1a049aaceae2c7b654743790faa8fd89b57fffe5
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-21 21:19:15 +0700

    Update fake.ps1

--- fake/fake.ps1
+++ fake/fake.ps1
@@ -1,16 +1,12 @@
 $url1 = "https://github.com/cia168168/folder/raw/main/fake/rans.mp4"
 $output1 = "$env:TEMP\rans.mp4"
 Invoke-WebRequest -Uri $url1 -OutFile $output1
-Start-Process $output1
 
 $url2 = "https://github.com/cia168168/folder/raw/main/fake/ransd.png"
 $output2 = "$env:TEMP\ransd.png"
 Invoke-WebRequest -Uri $url2 -OutFile $output2
-Start-Process $output2
 
-# Wait for processes to become idle (adjust the timeout as needed)
-Start-Sleep -Seconds 5
-
-# Send F11 key
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.SendKeys]::SendWait("{F11}")
+$url3 = "https://github.com/cia168168/folder/raw/main/fake/ransd.png"
+$output3 = "$env:TEMP\ransd.png"
+Invoke-WebRequest -Uri $url3 -OutFile $output3
+Start-Process $output3
