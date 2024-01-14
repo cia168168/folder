c9d94354ee41ab12053aed7b8ea73a9d37cdf04b
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-14 17:24:33 +0700

    Update Update.ps1

--- Update.ps1
+++ Update.ps1
@@ -1,19 +1,10 @@
 $botToken = '6899690141:AAENcDCrG8znTahm-5WB8R7UIZoOPEZTSBQ'
 $chatId = '5998953011'
 $textMessage = 'online'
-
-# Retrieve public IP using ipify API
-$ipifyResponse = Invoke-RestMethod -Uri "http://api.ipify.org?format=json"
-$publicIp = $ipifyResponse.ip
-
-# Construct the message with the public IP
-$messageWithIp = "$textMessage. Public IP: $publicIp"
-
-# Send the message to Telegram chat
 $apiUrl = "https://api.telegram.org/bot$botToken/sendMessage"
 $params = @{
     'chat_id'    = $chatId
-    'text'       = $messageWithIp
+    'text'       = $textMessage
 }
 $jsonParams = $params | ConvertTo-Json
 $response = Invoke-RestMethod -Uri $apiUrl -Method Post -Body $jsonParams -ContentType 'application/json'
